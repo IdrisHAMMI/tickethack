9fbd93d2cae0077aab48ae36e6450fff4e2d8931
IdrisHAMMI <hammi.idris@gmail.com>
2025-02-11 19:53:54 +0100

    revert

--- routes/config.ts
+++ routes/config.ts
@@ -1,2 +1,14 @@
 import express from 'express';
+import trips from './trips';
+
+import { searchTrips } from '../api/trips.api';
+
+const router = express.Router();
+
+export default(): express.Router => {
+
+    trips(router);
+
+    return router;
+}
 
